0b465e5db581c6d2a3cb431acfae345c053ea98d
LCL121 <47299089+LCL121@users.noreply.github.com>
2020-04-05 20:46:25 +0800

    project init

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,17 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import Home from '@/pages/home/Home'
 
 Vue.use(Router)
 
+// 导出的是路由的配置项
 export default new Router({
   routes: [
     {
+      // 当用户访问根路径的时候，给用户展示的是HelloWorld组件
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: 'Home',
+      component: Home
     }
   ]
 })
