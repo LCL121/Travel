d7d65c0fdeff8e50bec1fb4cb0492fc7b6637bb4
LCL121 <1391436522@qq.com>
2020-04-07 18:12:27 +0800

    search finish

--- src/pages/city/components/Search.vue
+++ src/pages/city/components/Search.vue
@@ -8,14 +8,24 @@
         v-model="keyword"
       />
     </div>
-    <div class="search-content">
+    <div
+      class="search-content"
+      ref="search"
+      v-show="keyword"
+    >
       <ul>
         <li
           class="search-item border-bottom"
           v-for="item of list"
-          :key="item.spell"
+          :key="item.id"
         >
           {{item.name}}
+        </li>
+        <li
+          class="search-item border-bottom"
+          v-show="hasNoData"
+        >
+          没有找到匹配数据
         </li>
       </ul>
     </div>
@@ -23,6 +33,7 @@
 </template>
 
 <script>
+import Bscroll from 'better-scroll'
 export default {
   name: 'CitySearch',
   props: {
@@ -35,10 +46,19 @@ export default {
       timer: null
     }
   },
+  computed: {
+    hasNoData () {
+      return !this.list.length
+    }
+  },
   watch: {
     keyword () {
       if (this.timer) {
         clearTimeout(this.timer)
+      }
+      if (!this.keyword) {
+        this.list = []
+        return
       }
       this.timer = setTimeout(() => {
         const result = []
@@ -53,6 +73,9 @@ export default {
         this.list = result
       }, 100)
     }
+  },
+  mounted () {
+    this.scroll = new Bscroll(this.$refs.search)
   }
 }
 </script>
