6b8c4f2a1dc404976fd323bed2ab844a8e07cffa
LCL121 <1391436522@qq.com>
2020-04-09 17:25:24 +0800

    detail finished

--- src/pages/detail/components/Banner.vue
+++ src/pages/detail/components/Banner.vue
@@ -17,16 +17,19 @@
         </div>
       </div>
     </div>
-    <common-gallary
-      :imgs="bannerImgs"
-      v-show="showGallary"
-      @close="handleGallaryClose"
-    ></common-gallary>
+    <fade-animation>
+      <common-gallary
+        :imgs="bannerImgs"
+        v-show="showGallary"
+        @close="handleGallaryClose"
+      ></common-gallary>
+    </fade-animation>
   </div>
 </template>
 
 <script>
 import CommonGallary from 'common/gallary/Gallary'
+import FadeAnimation from 'common/fade/FadeAnimation'
 export default {
   name: 'Banner',
   props: {
@@ -48,7 +51,8 @@ export default {
     }
   },
   components: {
-    CommonGallary
+    CommonGallary,
+    FadeAnimation
   }
 }
 </script>
