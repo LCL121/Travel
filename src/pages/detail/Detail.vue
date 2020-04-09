a945750b8bc397106e0e8ec453b86fa4379463bd
LCL121 <1391436522@qq.com>
2020-04-09 17:09:53 +0800

    detail list

--- src/pages/detail/Detail.vue
+++ src/pages/detail/Detail.vue
@@ -1,11 +1,11 @@
 <template>
   <div>
-    <detail-header></detail-header>
     <detail-banner
       :sightName="sightName"
       :bannerImg="bannerImg"
       :bannerImgs="gallaryImgs"
     ></detail-banner>
+    <detail-header></detail-header>
     <div class="content">
       <detail-list :list="list"></detail-list>
     </div>
